76ae50a354e70445a651045676db12467c81e8a2
lakshya1333 <lakshyajain906@gmail.com>
2025-01-30 15:06:58 +0530

    Added the routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,4 +1,29 @@
 import { Router } from 'express';
 const router = Router();
 
+/**
+ * @route   POST /api/auth/admin/login
+ * @desc    
+ * @access  
+ * @req     
+ * @res          
+ */
+
+router.post("/admin/login",(req,res)=>{
+    
+})
+
+/**
+ * @route   POST /api/auth/admin/logout
+ * @desc    
+ * @access  
+ * @req     
+ * @res          
+ */
+
+router.post("/admin/logout",(req,res)=>{
+    
+})
+
+
 export default router;
